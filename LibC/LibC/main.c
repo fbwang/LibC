2f8324bf538a17f759b2d88e1d6e47c86686d318
Crystal <crystal@CrystaldeMacBook-Pro.local>
2017-04-30 23:00:37 +0800

    min print function

---
+++ LibC/LibC/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LibC
+//
+//  Created by Crystal on 2017/4/30.
+//  Copyright © 2017年 Crystal. All rights reserved.
+//
+
+//#include <stdio.h>
+#include "print.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+//    printf("Hello, World!\n");
+    int i = 10;
+    print("%d\n", i);
+    return 0;
+}
---
+++ LibC/LibC/print.h
@@ -0,0 +1,15 @@
+//
+//  print.h
+//  LibC
+//
+//  Created by Crystal on 2017/4/30.
+//  Copyright © 2017年 Crystal. All rights reserved.
+//
+
+#ifndef print_h
+#define print_h
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#endif /* print_h */
